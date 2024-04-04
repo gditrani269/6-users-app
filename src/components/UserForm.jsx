9aa7248ff3393edeb04a748932c651f0ba9c0c3e
unknown <gditrani269@gmail.com>
2024-04-04 20:38:11 -0300

    seccion 7 - 105.Maquetacion formulario

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,8 +1,52 @@
+import { useState } from "react"
+
+const initialUserForm = {
+    username: '',
+    password: '',
+    email: ''
+}
+
 export const UserForm = () => {
 
+    const [userForm, setUserForm ] = useState (initialUserForm);
+
+    const { username, password, email } = userForm;
+
+    const onInputChange = ({ target }) => {
+ //       console.log (target.value)
+        const { name, value } = target;
+        setUserForm ({
+            ...userForm,
+            [name]: value,
+        })
+    }
+
     return (
-        <>
-            <p>Formulario de Usuarios</p>
-        </>
+        <form>
+            <input
+                className="form-control my-3 w-75"
+                placeholder="username"
+                name="username"
+                value= {username}
+                onChange={ onInputChange } />
+            <input
+                className="form-control my-3 w-75"
+                placeholder="password"
+                type="password"
+                name="password"
+                value={password}
+                onChange={ onInputChange } />
+            <input
+                className="form-control my-3 w-75"
+                placeholder="email" 
+                name="email"
+                value={email}
+                onChange={ onInputChange }/>
+            <button
+                className="btn btn-primary"
+                type="submit">
+                Crear
+            </button>
+        </form>
     );
 }

1e31430e7997dffed6f9ccabf71116b55a581f9e
unknown <gditrani269@gmail.com>
2024-04-04 16:35:22 -0300

    seccion 7 - 103.Maquetacion HTML y CSS

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,8 +1,18 @@
+import { UserForm } from "./components/UserForm";
+import { UsersList } from "./components/UsersList";
 
 export const UsersApp = () => {
     return (
         <div className="container my-4">
             <h2>Users App</h2>
+            <div className="row">
+                <div className="col">
+                    <UserForm />
+                </div>
+                <div className="col">
+                    <UsersList />
+                </div>
+            </div>
         </div>
     );
 }
---
+++ src/components/UsersList.jsx
@@ -0,0 +1,11 @@
+export const UsersList = () => {
+
+    return (
+        <>
+            <p>Listado de usuarios</p>
+            <table>
+
+            </table>
+        </>
+    );
+}

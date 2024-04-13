181402f71e2ce2969478a1f0ef10608431468b3f
unknown <gditrani269@gmail.com>
2024-04-13 14:46:36 -0300

    seccion 7 - 121.Componente LoginPage

---
+++ src/auth/pages/LoginPage.jsx
@@ -0,0 +1,33 @@
+export const LoginPage = () => {
+    return (
+        <div className="modal" style={ {display: "block"}} tabIndex="-1">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">Login Page</h5>
+                    </div>
+                    <form>
+                        <div className="modal-body">
+                            <input 
+                                className="form-control my-3 w-75"
+                                placeholder="Username"
+                                name="username"/>
+                            <input 
+                                className="form-control my-3 w-75"
+                                placeholder="password"
+                                name="password"
+                                type="password"/>
+                        </div>
+                        <div className="modal-footer">
+                            <button  className="btn btn-primary"
+                                type="submit">
+                                Login
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+
+}

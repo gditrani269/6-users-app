b0e4443c42058f6ed58675fcc206cd59773fed88
unknown <gditrani269@gmail.com>
2024-04-13 15:30:24 -0300

    seccion 7 - 123.Implementando eventos Login form

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,6 +1,8 @@
 import { LoginPage } from "./auth/pages/LoginPage";
 import { UsersPage } from "./pages/UsersPage";
 
+//si estamos autenticados va a cargar la UserPage
+//si NO estamos autenticados va a cargar la pagin LoginPage
 export const UsersApp = () => {
 
     return (
--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,38 @@
+import { useState } from "react";
+import Swal from "sweetalert2";
+
+const initialLoginForm = {
+    username: '',
+    password: '',
+}
+
 export const LoginPage = () => {
+
+    const [ LoginForm,setLoginForm ] = useState (initialLoginForm);
+    const { username, password } = LoginForm;
+
+    const onInputChange = ({target}) => {
+        const {name, value} = target;
+        setLoginForm ({
+            ...LoginForm,
+            [ name ]: value,
+        })
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault (); //esto es para que no se recargue la pagian cuandose pulsa el boton
+        if (!username || !password) {
+            Swal.fire ('Error de validacion', 'Username y ppasword requeridos', 'error');
+        }
+        //aca implementamos el login
+        if (username === 'admin' && password === '12345') {
+            //handler login
+        } else {
+            Swal.fire ('Error login', 'Username y/o ppasword invalidos', 'error');
+        }
+        //reiniciar los datos del formulario
+        setLoginForm (initialLoginForm);
+    }
     return (
         <div className="modal" style={ {display: "block"}} tabIndex="-1">
             <div className="modal-dialog">
@@ -6,17 +40,21 @@ export const LoginPage = () => {
                     <div className="modal-header">
                         <h5 className="modal-title">Login Page</h5>
                     </div>
-                    <form>
+                    <form onSubmit={ onSubmit }>
                         <div className="modal-body">
                             <input 
                                 className="form-control my-3 w-75"
                                 placeholder="Username"
-                                name="username"/>
+                                name="username"
+                                value={username}
+                                onChange={ onInputChange }/>
                             <input 
                                 className="form-control my-3 w-75"
                                 placeholder="password"
                                 name="password"
-                                type="password"/>
+                                type="password"
+                                value={password}
+                                onChange={ onInputChange }/>
                         </div>
                         <div className="modal-footer">
                             <button  className="btn btn-primary"

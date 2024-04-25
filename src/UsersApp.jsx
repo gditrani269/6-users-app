dd1f37a64fcc3c7b4a92f38df1d7d5baac492a67
unknown <gditrani269@gmail.com>
2024-04-25 16:02:02 -0300

    seccion 9 - 141 y 142.Creando el contexto auth y accediendo al estado y data de AuthContext

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,32 +1,27 @@
 
 import { LoginPage } from "./auth/pages/LoginPage";
-import { useAuth } from "./auth/hooks/useAuth";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { UserRoutes } from "./routes/UserRoutes";
-
+import { useContext } from "react";
+import { AuthContext } from "./auth/context/AuthContext";
 
 //si estamos autenticados va a cargar la UserPage
 //si NO estamos autenticados va a cargar la pagin LoginPage
 
 export const UsersApp = () => {
 
-    const { login, handlerLogin, handlerLogout} = useAuth ();
-    
+    const { login } = useContext (AuthContext)
     return (
         <Routes>
             {/*Cuando no este autenticado, cualquier ruta lo va a llegar al login por el <Route path="/*" element={ <Navigate to="/login" />} /> */}
             {
                 login.isAuth
                     ? (
-                        <Route path="/*" element={ <UserRoutes 
-                            login={login} 
-                            handlerLogout={handlerLogout}/>} />
+                        <Route path="/*" element={ <UserRoutes />} />
                     )
                     : <>
-                        <Route path="/login" 
-                            element={<LoginPage 
-                                handlerLogin={ handlerLogin }/>} />
-                                <Route path="/*" element={ <Navigate to="/login" />} />
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/*" element={ <Navigate to="/login" />} />
                     </>
                     
             }
--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -5,23 +5,19 @@ import { RegisterPage } from "../pages/RegisterPage"
 import { useUsers } from "../hooks/useUsers"
 import { UserProvider } from "../context/UserProvider"
 
-export const UserRoutes = ({ login, handlerLogout }) => {
+export const UserRoutes = () => {
 
     return (
 
         <>
             <UserProvider> {/*toda la informacion que esta aca se va a compartir por medio del context que nos da el UserProvide
              */}
-                <NavBar login={ login } handlerLogout={ handlerLogout }/>
+                <NavBar />
                 <Routes>
                     <Route path="users" element={<UsersPage />} />
-
                     <Route path="users/register" element={<RegisterPage />} />
-
                     <Route path="users/edit/:id" element={<RegisterPage  />} />
-
                     <Route path="/" element={<Navigate to="/users" />} />
-
                 </Routes>
             </UserProvider>
         </>

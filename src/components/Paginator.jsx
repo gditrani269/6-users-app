ad11a444d552be76ad8101f7e92ba17036e470ce
unknown <gditrani269@gmail.com>
2024-07-29 15:30:04 -0300

    Seccion 22 - 280. Creando componente Paginator e implementando links de las...

---
+++ src/components/Paginator.jsx
@@ -0,0 +1,25 @@
+import { useUsers } from "../hooks/useUsers"
+
+export const Paginator = () => {
+
+    const { paginator } = useSelector (state => state.users);
+    return (
+        <>
+            { paginator?.length === 0 ||
+                <ul className="pagination">
+                    {paginator.number == 0 || 
+                        <li className="page-item">
+                            <Link className="page-link" to ={`/user/page/${paginator.number-1}`}>atrás</Link>
+                        </li>
+                    }
+                    
+                    {paginator.number >= paginator.totalPages - 1 || 
+                        <li className="page-item">
+                            <Link className="page-link" to ={`/user/page/${paginator.number+1}`}>siguiente</Link>
+                        </li>
+                    }
+                </ul>
+            }
+        </>
+    )
+}
